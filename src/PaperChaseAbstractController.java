1e2207fe26b4db5a102bb4ab6b8e1a06e96dfd6b
RasmusGregersen <rasmus@gregersen.dk>
2017-03-01 11:53:58 +0100

    PaperChase and depencies imported

---
+++ src/PaperChaseAbstractController.java
@@ -0,0 +1,26 @@
+import com.google.zxing.Result;
+import de.yadrone.base.IARDrone;
+
+public abstract class PaperChaseAbstractController extends Thread implements TagListener
+{
+	protected boolean doStop = false;
+
+	protected IARDrone drone;
+	
+	public PaperChaseAbstractController(IARDrone drone)
+	{
+		this.drone = drone;
+	}
+
+	public abstract void run();
+	
+	public void onTag(Result result, float orientation)
+	{
+
+	}
+	
+	public void stopController()
+	{
+		doStop = true;
+	}
+}
---
+++ src/TagListener.java
@@ -0,0 +1,8 @@
+import com.google.zxing.Result;
+
+public interface TagListener
+{
+
+	public void onTag(Result result, float orientation);
+	
+}
